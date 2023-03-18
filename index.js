2a9aabebc0b88eec7b690907723dda52b2541268
Eshratjahan17 <eshratjahan701@gmail.com>
2023-03-18 23:09:44 +0600

    1st commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port =process.env.PORT|| 5000;
+const cors = require("cors");
+require("dotenv").config();
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.DB_PASS}@cluster0.bdfv668.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+try{
+    await client.connect();
+    const EventsCollection = client.db("mussc").collection("events");
+
+}
+finally{
+
+}
+console.log("Database connected");
+
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
